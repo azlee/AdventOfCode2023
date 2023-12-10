8fa709e12581315fedb3d6b5005ad7927bbf677a
Amber Lee <amber_Lee1993@hotmail.com>
2023-12-10 14:28:18 -0800

    day 4 part 2

--- day04.ts
+++ day04.ts
@@ -32,4 +32,38 @@ function getPoints(): number {
   return points;
 }
 
+function part2(): number {
+  const input = fs.readFileSync("day04.input", "utf8");
+  let points = 0;
+  const lines = input.split("\n");
+  const copiesArray = Array(lines.length).fill(1);
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const [winningNumbersStr, yourNumbersStr] = line
+      .split(":")[1]
+      .trim()
+      .split("|")
+      .map((s) => s.trim());
+    const winningNumbersSet = new Set(
+      winningNumbersStr
+        .split(" ")
+        .filter((x) => x != "")
+        .map((s) => s.trim())
+    );
+    const yourNumbers = yourNumbersStr
+      .split(" ")
+      .filter((x) => x != "")
+      .map((s) => s.trim());
+    let winningAmount = 0;
+    for (const number of yourNumbers) {
+      if (winningNumbersSet.has(number)) winningAmount++;
+    }
+    for (let j = i + 1; j <= i + winningAmount; j++) {
+      copiesArray[j] += copiesArray[i];
+    }
+  }
+  return copiesArray.reduce((a, x) => a + x, 0);
+}
+
 console.log(getPoints());
+console.log(part2());
