7ed1ed13c2441417843234751f7bae72c40564d4
Amber Lee <amber_Lee1993@hotmail.com>
2023-12-14 23:42:07 -0800

    day 15 part 2

--- day15.ts
+++ day15.ts
@@ -21,4 +21,54 @@ function part1() {
   return sum;
 }
 
+function part2() {
+  const input = fs.readFileSync("day15.input", "utf8").split(",");
+  const boxes: { [key: number]: string[] } = {};
+  for (const step of input) {
+    if (step.indexOf("=") >= 0) {
+      const [label, focalLength] = step.split("=");
+      const boxNum = getResult(label);
+      const lensInBox = boxes[boxNum] || [];
+      let index = -1;
+      for (let i = 0; i < lensInBox.length; i++) {
+        const lens = lensInBox[i];
+        if (lens.startsWith(label)) {
+          index = i;
+        }
+      }
+      if (index >= 0) {
+        lensInBox[index] = `${label} ${focalLength}`;
+      } else {
+        lensInBox.push(`${label} ${focalLength}`);
+      }
+      boxes[boxNum] = lensInBox;
+    } else {
+      const [label] = step.split("-");
+      const boxNum = getResult(label);
+      const lensInBox = boxes[boxNum] || [];
+      let index = -1;
+      for (let i = 0; i < lensInBox.length; i++) {
+        const lens = lensInBox[i];
+        if (lens.startsWith(label)) {
+          index = i;
+        }
+      }
+      if (index >= 0) {
+        lensInBox.splice(index, 1);
+      }
+      boxes[boxNum] = lensInBox;
+    }
+  }
+  let sum = 0;
+  for (const [boxNum, entries] of Object.entries(boxes)) {
+    for (let i = 0; i < entries.length; i++) {
+      const focalLength = Number(entries[i].split(" ")[1]);
+      console.log(Number(boxNum) + 1, i + 1, focalLength);
+      sum += (Number(boxNum) + 1) * (i + 1) * focalLength;
+    }
+  }
+  return sum;
+}
+
 console.log(part1());
+console.log(part2());
