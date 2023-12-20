ff9ba5d5e674d738dc0dc06d4c0040b8b226babd
Amber Lee <amber_Lee1993@hotmail.com>
2023-12-19 22:59:36 -0800

    day 17 part 2

--- day17.ts
+++ day17.ts
@@ -19,15 +19,20 @@ type Step = {
   numStepsInDir: number;
 };
 
-function part(): any {
+function part(isPart1: boolean): any {
   const input = getInput();
   const seen: Set<string> = new Set();
   // heat loss, position row, position col, dir row, dir col, # of steps in dir
   const pq: PriorityQueue<number[]> = priorityQueue();
   pq.insert([0, 0, 0, 0, 0, 0]);
+  const maxSteps = isPart1 ? 3 : 10;
   while (!pq.isEmpty()) {
     const [hl, r, c, dr, dc, n] = pq.pop();
-    if (r === input.length - 1 && c === input[0].length - 1) {
+    if (
+      r === input.length - 1 &&
+      c === input[0].length - 1 &&
+      (isPart1 || n >= 4)
+    ) {
       return hl;
     }
     if (r < 0 || c < 0 || r >= input.length || c >= input[0].length) {
@@ -38,7 +43,7 @@ function part(): any {
       continue;
     }
     seen.add(setStr);
-    if (n < 3 && "00" !== [dr, dc].join("")) {
+    if (n < maxSteps && "00" !== [dr, dc].join("")) {
       const nr = r + dr;
       const nc = c + dc;
       // continue in direction
@@ -46,25 +51,28 @@ function part(): any {
         pq.insert([hl + input[nr][nc], nr, nc, dr, dc, n + 1]);
       }
     }
-    for (const [ndr, ndc] of [
-      [0, 1],
-      [1, 0],
-      [0, -1],
-      [-1, 0],
-    ]) {
-      if (
-        [ndr, ndc].join("") !== [dr, dc].join("") &&
-        [ndr, ndc].join("") !== [-dr, -dc].join("")
-      ) {
-        const nr = r + ndr;
-        const nc = c + ndc;
-        // new direction
-        if (nr >= 0 && nr < input.length && nc >= 0 && nc < input.length) {
-          pq.insert([hl + input[nr][nc], nr, nc, ndr, ndc, 1]);
+    if (isPart1 || "00" === [dr, dc].join("") || (n >= 4 && n < maxSteps)) {
+      for (const [ndr, ndc] of [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+      ]) {
+        if (
+          [ndr, ndc].join("") !== [dr, dc].join("") &&
+          [ndr, ndc].join("") !== [-dr, -dc].join("")
+        ) {
+          const nr = r + ndr;
+          const nc = c + ndc;
+          // new direction
+          if (nr >= 0 && nr < input.length && nc >= 0 && nc < input.length) {
+            pq.insert([hl + input[nr][nc], nr, nc, ndr, ndc, 1]);
+          }
         }
       }
     }
   }
 }
 
-console.log(part());
+// console.log(part(true));
+console.log(part(false));
